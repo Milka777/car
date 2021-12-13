bc09232ffa2b13eacae4573b52c779c59042875c
Milka777 <82575185+Milka777@users.noreply.github.com>
2021-12-13 14:01:45 +0300

    Add files via upload

---
+++ drive/drive/ChangeCarCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ChangeCarCollectionViewCell.swift
+//  Raingosling
+//
+//  Created by user on 13.12.2021.
+//
+
+import UIKit
+
+class ChangeCarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var carPicture: UIImageView!
+
+    
+}
